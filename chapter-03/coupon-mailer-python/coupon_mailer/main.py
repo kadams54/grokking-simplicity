3f0f78e84760aed979ba564f77dbad17397725ce
Kyle Adams <kadams54@users.noreply.github.com>
2023-06-06 13:11:03 -0400

    Initial setup

---
+++ chapter-03/coupon-mailer-python/coupon_mailer/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+
+EMAILS_TABLE = [
+    {"email": "john@coldmail.com", "rec_count": 2},
+    {"email": "sam@pmail.co", "rec_count": 16},
+    {"email": "linda1989@oal.com", "rec_count": 1},
+    {"email": "jan1940@ahoy.com", "rec_count": 0},
+    {"email": "mrbig@pmail.co", "rec_count": 25},
+    {"email": "lol@lol.lol", "rec_count": 0},
+]
+
+COUPONS_TABLE = [
+    {"coupon": "MAYDISCOUNT", "rank": "good"},
+    {"coupon": "10PERCENT", "rank": "bad"},
+    {"coupon": "PROMOTION45", "rank": "best"},
+    {"coupon": "IHEARTYOU", "rank": "bad"},
+    {"coupon": "GETADEAL", "rank": "best"},
+    {"coupon": "ILIKEDISCOUNTS", "rank": "good"},
+]
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
