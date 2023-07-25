7860eb95f9e87c0c2bc941ad5e20e5afd2e84f7d
Kyle Adams <kadams54@users.noreply.github.com>
2023-07-25 13:25:47 -0400

    Initial code

--- chapter-06/shopping-cart-python/shopping_cart/__main__.py
+++ chapter-06/shopping-cart-python/shopping_cart/__main__.py
@@ -1,4 +1,72 @@
 import sys
+from typing import TypedDict, TypeVar
+
+from .external import get_buy_buttons_dom, set_cart_total_dom, set_tax_dom
+
+
+class Item(TypedDict):
+    name: str
+    price: float
+
+
+T = TypeVar("T")
+shopping_cart: list[Item] = []
+
+def add_item_to_cart(name: str, price: float) -> None:
+    global shopping_cart
+    item = make_cart_item(name, price)
+    shopping_cart = add_item(shopping_cart, item)
+    total = calc_total(shopping_cart)
+    set_cart_total_dom(total)
+    update_shipping_icons(shopping_cart)
+    update_tax_dom(total)
+
+
+def update_shipping_icons(cart: list[Item]) -> None:
+    buttons = get_buy_buttons_dom()
+    for button in buttons:
+        item = button.item
+        new_cart = add_item(cart, item)
+        if gets_free_shipping(new_cart):
+            button.show_free_shipping_icon()
+        else:
+            button.hide_free_shipping_icon()
+
+
+def update_tax_dom(total: float) -> None:
+    set_tax_dom(calc_tax(total))
+
+
+def add_element_last(array: list[T], item: T) -> list[T]:
+    new_array = array[:]
+    new_array.append(item)
+    return new_array
+
+
+def add_item(cart: list[Item], item: Item) -> list[Item]:
+    return add_element_last(cart, item)
+
+
+def make_cart_item(name: str, price: float) -> Item:
+    return {
+        "name": name,
+        "price": price
+    }
+
+
+def calc_total(cart: list[Item]) -> float:
+    total = 0
+    for item in cart:
+        total += item.price
+    return total
+
+
+def gets_free_shipping(cart: list[Item]) -> bool:
+    return calc_total(cart) >= 20
+
+
+def calc_tax(amount: float) -> float:
+    return amount * 0.10
 
 
 def main() -> int:
---
+++ chapter-06/shopping-cart-python/shopping_cart/external.py
@@ -0,0 +1,19 @@
+class Button:
+    def show_free_shipping_icon(self) -> None:
+        pass
+
+
+    def hide_free_shipping_icon(self) -> None:
+        pass
+
+
+def set_cart_total_dom(total: float) -> None:
+    pass
+
+
+def get_buy_buttons_dom() -> list[Button]:
+    return [Button()]
+
+
+def set_tax_dom(total: float) -> None:
+    pass
