c29dd64b2580d865270e40b22c9998182b23ebc9
Kyle Adams <kadams54@users.noreply.github.com>
2023-10-10 13:55:41 -0400

    Comment unused concat function in JS impl

--- chapter-13/compute/compute.js
+++ chapter-13/compute/compute.js
@@ -11,6 +11,9 @@ function repeat(n, times) {
   return Array(times).fill(n);
 }
 
+// JS has a flatMap() that combines mapping with flattening. Flattening is what
+// reduce + this concat function would do, so we don't really need this
+// function in JS.
 function concat(list_a, list_b) {
   return [...list_a, ...list_b]
 }
